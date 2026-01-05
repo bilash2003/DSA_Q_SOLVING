e9b60a7f3ce91b2c85a9d4e01442c8cfe6167d9a
Bilash Mallick <138954227+bilash2003@users.noreply.github.com>
2026-01-05 10:29:51 +0530

    Implement Boyer–Moore Majority Vote Algorithm
    Refactor majorityElement function to use Boyer–Moore Majority Vote Algorithm for improved efficiency.

--- Array/MAP/Majority Element II.cpp
+++ Array/MAP/Majority Element II.cpp
@@ -15,7 +15,8 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
- 
+        // // my code...
+
         int n = nums.size() / 3;
         unordered_map<int, int> um;
         for (int x : nums) {
@@ -28,5 +29,40 @@ public:
                 nums.push_back(key);
         }
         return nums;
+
+        //............................................
+        // Boyer–Moore Majority Vote Algorithm (n/3 version)
+        // at most 2 such elements and only 
+        // need two candidates + two counters
+        int c1=0,c2=0; //counter
+        int a=0,b=1; //candidates..
+        //1st pass....
+        for(int x : nums){
+            if(x==a)c1++;
+            else if(x==b)c2++;
+            else if(c1==0){
+                a=x;
+                c1=1;
+            }
+            else if(c2==0){
+                b=x;
+                c2=1;
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+        // 2nd pass for count
+        c1=0;c2=0;
+        for(int x:nums){
+            if(x==a)c1++;
+            else if(x==b)c2++;
+        }
+        vector<int>res;
+        if(c1>nums.size()/3)res.push_back(a);
+        if(c2>nums.size()/3)res.push_back(b);
+        return res;
+        
     }
 };

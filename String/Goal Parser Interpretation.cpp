c6a4f88648d41ae3a35a1d4bbbb23f058ee946ee
Bilash Mallick <138954227+bilash2003@users.noreply.github.com>
2026-01-25 19:19:01 +0530

    Fix output comment in Goal Parser Interpretation

---
+++ String/Goal Parser Interpretation.cpp
@@ -0,0 +1,35 @@
+#Question
+// 1678. Goal Parser Interpretation - Easy
+
+// You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+// Given the string command, return the Goal Parser's interpretation of command.
+// Example 1:
+// Input: command = "G()(al)"
+// Output: "Goal"
+// Explanation: The Goal Parser interprets the command as follows:
+// G -> G
+// () -> o
+// (al) -> al
+// The final concatenated result is "Goal".
+// Example 2:
+// Input: command = "G()()()()(al)"
+// Output: "Gooooal"
+// Example 3:
+// Input: command = "(al)G(al)()()G"
+// Output: "alGalooG"
+
+#solution
+class Solution {
+public:
+    string interpret(string command) {
+        string res = "";
+        for (int i = 1; i <= command.size(); i++) {
+            if (command[i - 1] == 'G' || command[i - 1] == 'a' ||
+                command[i - 1] == 'l') {
+                res = res + command[i - 1];
+            } else if (command[i - 1] == '(' && command[i] == ')')
+                res = res + "o";
+        }
+        return res;
+    }
+};

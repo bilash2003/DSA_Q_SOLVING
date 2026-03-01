8173ea228e4bd7f6b9a80b5ab27ca17bef880d84
Bilash Mallick <138954227+bilash2003@users.noreply.github.com>
2026-03-01 08:45:50 +0530

    Add solution for finding N unique integers summing to zero
    Implemented a solution to generate N unique integers that sum to zero, handling both even and odd cases.

---
+++ Array/Math/Find N Unique Integers Sum up to Zero.cpp
@@ -0,0 +1,33 @@
+#question
+// 1304. Find N Unique Integers Sum up to Zero - Easy
+// Given an integer n, return any array containing n unique integers such that they add up to 0.
+// Example 1:
+// Input: n = 5
+// Output: [-7,-1,1,3,4]
+// Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+// Example 2:
+// Input: n = 3
+// Output: [-1,0,1]
+// Example 3:
+// Input: n = 1
+// Output: [0]
+// Constraints:
+// 1 <= n <= 1000
+
+#Solution
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        set<int> res;
+        srand(time(0));
+        if (n % 2 != 0)
+            res.insert(0);
+        while (res.size() != n) {
+            int n = rand() % 1001;
+            res.insert(n);
+            res.insert(-n);
+        }
+        vector<int>v(res.begin(),res.end());
+        return v;
+    }
+};

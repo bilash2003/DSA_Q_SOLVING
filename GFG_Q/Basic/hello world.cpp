5953d4aab7ce68c3c1822f168d98dc71c3c801c4
Bilash Mallick <138954227+bilash2003@users.noreply.github.com>
2026-03-17 20:45:49 +0530

    Create hello world program in C++
    Added a C++ program that prints 'Hello World'.

---
+++ GFG_Q/Basic/hello world.cpp
@@ -0,0 +1,22 @@
+#Question
+// C++ Hello World
+// Let's begin your journey towards coding with the very first question of the coding world. Your task is to write code that prints Hello World.
+// Example 1:
+// Input:
+// No Input
+// Output:
+// Hello World
+
+#solution
+
+
+class Solution{
+    public:
+    void helloWorld()
+    {
+    
+        //Type below cout<<"Hello World"; 
+        //cout will output to console
+        cout<<"Hello World";
+    }
+};
